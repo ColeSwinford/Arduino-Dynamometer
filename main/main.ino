f599bdd7414aeb136bd5a68501d7e3ed948ab821
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-10-11 09:23:58 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,24 @@
+#define PI 3.1415926535897932384626433832795
+
 //vars
 int hallSensor =  2;
-float rev = 0 ;
-unsigned int rpm ;
-unsigned long prevTime = 0 ;
+float rev = 0;
+unsigned int rpm;
+unsigned long prevTime = 0;
 unsigned long int time;
-float torque ;
-float hp ;
+float angularVel0 = 0;
+float angularVel1;
+float angularAccel;
+// float torque;
+float hp;
 
-//constants
-alpha = 1234;
-momentInteria = 1234;
+////// testing for momentInertia constant
+momentInertia;
+float torque = 1234;
+////// testing for momentInertia constant
+
+//consts
+// momentInertia = 1234;
 
 void interruptRevCounter()  //interrupt service routine
 {
@@ -28,8 +37,16 @@ void loop()
   detachInterrupt(digitalPinToInterrupt(hallSensor)); //detach interrupt
   time = micros() - prevTime;  //get time elapsed since last check
   rpm = ( rev / time ) * 60000000;  //calculate rpm
+  angularVel1 = (rpm / 60) * (2 * PI);  //calculate angularVel1 in rad/s
+  angularAccel = (angularVel1 - angularVel0)/(time - prevTime);  //calculate angularAccel
+  angularVel0 = angularVel1;  //save angularVel0
   prevTime = micros();  //save current time
-  torque = alpha * momentInteria;  //calculate torque
+  // torque = angularAccel * momentInertia;  //calculate torque
+
+  ////// testing for momentInertia constant
+  momentInertia = torque / angularAccel;
+  ////// testing for momentInertia constant
+  
   hp = (rpm * torque) / 63025  //calculate hp
   rev = 0;
 
@@ -43,9 +60,14 @@ void serialDisplay()
 {
   Serial.print(rpm);
   Serial.print(",");
-  Serial.print(torque);
-  Serial.print(",");
-  Serial.print(hp);
-  Serial.print(",");
+  // Serial.print(torque);
+  // Serial.print(",");
+  // Serial.print(hp);
+  // Serial.print(",");
+
+  ////// testing for momentInertia constant
+  Serial.print(momentInertia);
+  ////// testing for momentInertia constant
+  
   Serial.println(micros()/1000000);
 }
