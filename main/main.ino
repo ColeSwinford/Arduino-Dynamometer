040efd36034705692aae962ac966d7090822f56e
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-10-11 08:43:08 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,15 @@
+//vars
 int hallSensor =  2;
 float rev = 0 ;
 unsigned int rpm ;
 unsigned long prevTime = 0 ;
 unsigned long int time;
+float torque ;
+float hp ;
+
+//constants
+alpha = 1234;
+momentInteria = 1234;
 
 void interruptRevCounter()  //interrupt service routine
 {
@@ -22,6 +29,8 @@ void loop()
   time = micros() - prevTime;  //get time elapsed since last check
   rpm = ( rev / time ) * 60000000;  //calculate rpm
   prevTime = micros();  //save current time
+  torque = alpha * momentInteria;  //calculate torque
+  hp = (rpm * torque) / 63025  //calculate hp
   rev = 0;
 
   serialDisplay();
@@ -33,6 +42,10 @@ void loop()
 void serialDisplay()
 {
   Serial.print(rpm);
+  Serial.print(",");
+  Serial.print(torque);
+  Serial.print(",");
+  Serial.print(hp);
   Serial.print(",");
   Serial.println(micros()/1000000);
 }
