0a74af481868f4c8bf87857bf112159403e827a8
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-10-22 17:52:31 -0400

    added linear interpolation

--- main/main.ino
+++ main/main.ino
@@ -2,20 +2,32 @@
 
 //vars
 int hallSensor =  2;
-float rev = 0;
+double rev = 0;
 unsigned int rpm;
 unsigned long prevTime = 0;
 unsigned long int time;
-float angularVel0 = 0;
-float angularVel1;
-float angularAccel;
-float torque;
-float curveHP;
-float hp;
+double angularVel0 = 0;
+double angularVel1;
+double angularAccel;
+double torque;
+double curveHP;
+double hp;
 
 ////// testing for momentInertia constant
-float momentInertia;
+double momentInertia;
 ////// testing for momentInertia constant
+
+//Kohler CH440 Torque and HP curve relative to RPM. {RPM, Torque, HP}
+double RPM_v_Torque_v_HP[][3]{
+  {0, 0, 0},
+  {2400, 18.5, 8.5},
+  {2600, 18.1, 9},
+  {2800, 17.4, 9.3},
+  {3000, 16.6, 9.5},
+  {3200, 15.4, 9.4},
+  {3400, 14.5, 9.4},
+  {3600, 13.5, 9.3}
+};
 
 //consts
 // momentInertia = 1234;
@@ -32,19 +44,82 @@ void setup()
 }
 
 // torque and hp calculations based on data from engine curves
-float getTorque(int rpm)
+double getTorque(int rpm)
 {
   // torque = y = 16.6 + 4.25E-03x + -1.43E-06x^2
-  float torque = 16.6 + (0.00425)*rpm + (-0.00000143)*(pow(rpm, 2));
+  // double torque = 16.6 + (0.00425)*rpm + (-0.00000143)*(pow(rpm, 2));
+  //y=
+  double y = rpm;
+  double y0;
+  int xLow;
+  int xUp;
+  float yLow;
+  float yUp;
+  if(rpm <= 2400)
+  {
+    xLow = RPM_v_Torque_v_HP[0][0];
+    yLow = RPM_v_Torque_v_HP[0][1];
+    xUp = RPM_v_Torque_v_HP[1][0];
+    yUp = RPM_v_Torque_v_HP[1][1];
+  }
+  if(rpm <= 2600 && rpm > 2400)
+  {
+    xLow = RPM_v_Torque_v_HP[1][0];
+    yLow = RPM_v_Torque_v_HP[1][1];
+    xUp = RPM_v_Torque_v_HP[2][0];
+    yUp = RPM_v_Torque_v_HP[2][1];
+  }
+  if(rpm <= 2800 && rpm > 2600)
+  {
+    xLow = RPM_v_Torque_v_HP[2][0];
+    yLow = RPM_v_Torque_v_HP[2][1];
+    xUp = RPM_v_Torque_v_HP[3][0];
+    yUp = RPM_v_Torque_v_HP[3][1];
+  }
+  if(rpm <= 3000 && rpm > 2800)
+  {
+    xLow = RPM_v_Torque_v_HP[3][0];
+    yLow = RPM_v_Torque_v_HP[3][1];
+    xUp = RPM_v_Torque_v_HP[4][0];
+    yUp = RPM_v_Torque_v_HP[4][1];
+  }
+  if(rpm <= 3200 && rpm > 3000)
+  {
+    xLow = RPM_v_Torque_v_HP[4][0];
+    yLow = RPM_v_Torque_v_HP[4][1];
+    xUp = RPM_v_Torque_v_HP[5][0];
+    yUp = RPM_v_Torque_v_HP[5][1];
+  }
+  if(rpm <= 3400 && rpm > 3200)
+  {
+    xLow = RPM_v_Torque_v_HP[5][0];
+    yLow = RPM_v_Torque_v_HP[5][1];
+    xUp = RPM_v_Torque_v_HP[6][0];
+    yUp = RPM_v_Torque_v_HP[6][1];
+  }
+  if(rpm <= 3600 && rpm > 3400)
+  {
+    xLow = RPM_v_Torque_v_HP[6][0];
+    yLow = RPM_v_Torque_v_HP[6][1];
+    xUp = RPM_v_Torque_v_HP[7][0];
+    yUp = RPM_v_Torque_v_HP[7][1];
+  }
+  if(rpm > 3600)
+  {
+    torque = 12345678910;
+  }
+
+  // y = yLow + slope(x-xLow), where slope = (yUp-YLow)/(xUp-xLow)
+  double torque = yLow + (((yUp-yLow)/(xUp-xLow))*(rpm-xLow));
   return torque;
-}
-float getHP(int rpm)
+};
+double getHP(int rpm)
 {
   // hp = y = -5.99 + 9.7E-03x + -1.52E-06x^2
-  float hp = -5.99 + (0.0097)*rpm + (-0.00000152)*(pow(rpm, 2));
+  double hp = -5.99 + (0.0097)*rpm + (-0.00000152)*(pow(rpm, 2));
   return hp;
 }
-float getMomentInertia(float torque, float angularAccel)
+double getMomentInertia(double torque, double angularAccel)
 {
   momentInertia = torque / angularAccel;
   return momentInertia;
